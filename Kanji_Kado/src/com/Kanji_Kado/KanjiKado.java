ba5d43e81e66e5d24fbdfdb1a6e872d2b58119f6
zen6 <bilal.akabi@gmail.com>
2013-12-02 23:57:35 -0500

    continuing w/ tutorial, World Renderer...

---
+++ Kanji_Kado/src/com/Kanji_Kado/KanjiKado.java
@@ -0,0 +1,16 @@
+package com.Kanji_Kado;
+
+import com.Kanji_Kado.GameScreen;
+import com.badlogic.gdx.Game;
+
+public class KanjiKado extends Game {
+
+	@Override
+	public void create() {
+		setScreen(new GameScreen());
+		//up to worldRenderer
+	}
+	
+	
+
+}
